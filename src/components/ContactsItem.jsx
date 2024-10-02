ce206e49da47b6dd7c9908f344b44f2415f1a879
Felix <felix.mathiasson99@gmail.com>
2024-10-02 13:53:13 +0200

    delete works

--- src/components/ContactsItem.jsx
+++ src/components/ContactsItem.jsx
@@ -1,6 +1,26 @@
 import {Link} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 export default function ContactsItem({contact, setContact}) {
+
+
+    const nav = useNavigate();
+
+    function ManageDelete() {
+        const dewIt = window.confirm(
+            `Do you want to delete ${contact.firstName} ${contact.lastName} permanently?`
+        )
+        if(dewIt) { 
+            fetch(`https://boolean-uk-api-server.fly.dev/FelixMathiasson/contact/${contact.id}`, {
+                method: "DELETE",
+            }).then(setContact).then(() => {
+                nav('/contacts')
+            })
+        }
+    }
+
+   
+
 
 
     return (
@@ -8,6 +28,9 @@ export default function ContactsItem({contact, setContact}) {
             <Link to={`/contacts/${contact.id}`} onClick={() =>  setContact(contact)}>
                 {contact.firstName} {contact.lastName}
             </Link>
+            <Link to='/'>
+				<button onClick={ManageDelete}>Delete Contact</button>
+			</Link>
         </li>
     )
 }
